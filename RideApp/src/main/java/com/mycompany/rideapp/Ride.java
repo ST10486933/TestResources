71ff50b9fc845ca4b25dc424071426c473364d60
ST10486933 <ST10486933@vcconnect.edu.za>
2025-09-18 19:41:02 +0200

    Add files via upload

---
+++ RideApp/src/main/java/com/mycompany/rideapp/Ride.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.rideapp;
+
+// Interface Class
+public interface Ride {
+    
+    public String getDriverName();
+    public int getPassengerCount();
+    public double calculateFare();
+}
