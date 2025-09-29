b524c585d835f18891fb9b6411d877ad4069fbbe
ST10486933 <ST10486933@vcconnect.edu.za>
2025-09-29 11:56:10 +0200

    Add files via upload

---
+++ RunApplication/src/main/java/com/mycompany/runapplication/RunApplication.java
@@ -0,0 +1,33 @@
+
+package com.mycompany.runapplication;
+
+import java.util.Scanner;
+
+// Main Class
+public class RunApplication {
+
+    // Main Method
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        // Method to enter vehicle Type
+        System.out.print("Enter the accident vehicle type: ");
+        String vehicleType = scanner.nextLine();
+        
+        // Method to enter City where accident occured
+        System.out.print("Enter the city for the vehicle accidents: ");
+        String city = scanner.nextLine();
+        
+        // Method to enter total of accidents in that city for that vehicle
+        System.out.print("Enter the total " + vehicleType + " accidents for " + city + ": ");
+        int numAccidents = scanner.nextInt();
+        scanner.nextLine();
+        
+        // Create RoadAccidentReport object with user input
+        RoadAccidentReport report = new RoadAccidentReport(vehicleType, city, numAccidents);
+        
+        // Print report
+        report.printAccidentReport();
+    }
+}
