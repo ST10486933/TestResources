62c1a0a0314627d307f4ab4b5cb0c6d64141b102
ST10486933 <ST10486933@vcconnect.edu.za>
2025-11-08 23:07:17 +0200

    Add files via upload

---
+++ MovieGUI/src/main/java/com/mycompany/moviegui/MovieTicketData.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.moviegui;
+
+public class MovieTicketData 
+{
+    public String movieName;
+    public int numberOfTickets;
+    public double ticketPrice;
+
+    // constructor
+    public MovieTicketData(String movieName, int numberOfTickets, double ticketPrice)
+    {
+        this.movieName = movieName;
+        this.numberOfTickets = numberOfTickets;
+        this.ticketPrice = ticketPrice;
+    }
+}
